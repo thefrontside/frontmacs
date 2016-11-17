0e0265cc4839e9355214bc1b9f35e737e0ad747f
robdel12 <robertdeluca19@gmail.com>
2016-11-17 16:14:56 -0600

    Add a proper gitignore

--- .emacs.d/elpa/quelpa-0.0.1/quelpa-pkg.el
+++
@@ -1,2 +0,0 @@
-;;; -*- no-byte-compile: t -*-
-(define-package "quelpa" "0.0.1" "Emacs Lisp packages built directly from source" '((package-build "0") (emacs "24.3")) :authors '(("steckerhalter")) :maintainer '("steckerhalter") :url "https://github.com/quelpa/quelpa")
--- Makefile
+++ Makefile
@@ -1,3 +1,7 @@
 clean:
 	rm -rf .emacs.d/elpa
 	rm -rf .emacs.d/smex-items
+	rm -rf .emacs.d/quelpa
+
+runlocal: clean
+	HOME=`pwd` emacs

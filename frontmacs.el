e19406c61289e9f64278d9e3e2f70f929ecfdd9d
robdel12 <robertdeluca19@gmail.com>
2016-11-17 16:11:52 -0600

    Add file header to frontmacs.el

--- frontmacs.el
+++ frontmacs.el
@@ -1,1 +1,9 @@
+;; Author: Frontside  <admin@frontside.io>
+;; Version: 0.1.0
+;; Maintainer: Frontside <admin@frontside.io>
+;; Keywords: emacs
+
+;;;###autoload
 (message "hello world")
+
+;;; frontmacs.el ends here

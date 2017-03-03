0998c9e33870ceb853c1a17aa8e1cf3452cdbbd5
robdel12 <robertdeluca19@gmail.com>
2017-03-03 16:34:26 -0600

    Don't hardcode the version number

--- scripts/build-archive.el
+++ scripts/build-archive.el
@@ -1,3 +1,3 @@
 (require 'package-x)
 (setq package-archive-upload-base (concat default-directory "dist/packages"))
-(package-upload-file (concat default-directory "dist/frontmacs-0.1.0.tar"))
+(package-upload-file (car (file-expand-wildcards "dist/frontmacs-*.tar")))

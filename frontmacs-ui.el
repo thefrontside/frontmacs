14cf33b2ebdc16fb02ad58d4235d06df0aa99d2f
Charles Lowell <cowboyd@frontside.io>
2017-03-03 08:53:57 -0600

    add basic frontmacs-ui to make it a multi-file package

---
+++ frontmacs-ui.el
@@ -0,0 +1,62 @@
+;;; frontmacs-ui.el --- Basic UI elements
+;;; Commentary:
+
+;;; Code:
+
+;; ;; the toolbar is just a waste of valuable screen estate
+;; ;; in a tty tool-bar-mode does not properly auto-load, and is
+;; ;; already disabled anyway
+;; (when (fboundp 'tool-bar-mode)
+;;   (tool-bar-mode -1))
+
+;; (menu-bar-mode -1)
+
+;; ;; the blinking cursor is nothing, but an annoyance
+;; (blink-cursor-mode -1)
+
+;; ;; disable the annoying bell ring
+;; (setq ring-bell-function 'ignore)
+
+;; ;; disable startup screen
+;; (setq inhibit-startup-screen t)
+
+;; ;; nice scrolling
+;; (setq scroll-margin 0
+;;       scroll-conservatively 100000
+;;       scroll-preserve-screen-position 1)
+
+;; ;; mode line settings
+;; (line-number-mode t)
+;; (column-number-mode t)
+;; (size-indication-mode t)
+
+;; ;; enable y/n answers
+;; (fset 'yes-or-no-p 'y-or-n-p)
+
+;; ;; more useful frame title, that show either a file or a
+;; ;; buffer name (if the buffer isn't visiting a file)
+;; (setq frame-title-format
+;;       '("" invocation-name " Frontmacs - " (:eval (if (buffer-file-name)
+;;                                             (abbreviate-file-name (buffer-file-name))
+;;                                           "%b"))))
+
+;; ;; use zenburn as the default theme
+
+;; (load-theme "zenburn-theme" t)
+
+;; (require 'smart-mode-line)
+;; (setq sml/no-confirm-load-theme t)
+;; ;; delegate theming to the currently active theme
+;; (setq sml/theme nil)
+;; (add-hook 'after-init-hook #'sml/setup)
+
+;; ;; show the cursor when moving after big movements in the window
+;; (require 'beacon)
+;; (beacon-mode +1)
+
+;; ;; show available keybindings after you start typing
+;; (require 'which-key)
+;; (which-key-mode +1)
+
+(provide 'frontmacs-ui)
+;;; frontmacs-ui.el ends here

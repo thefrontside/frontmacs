6a3467b0bdb9735d65300afd0f78a162adc0f47f
Charles Lowell <cowboyd@frontside.io>
2017-03-13 11:13:20 -0500

    Bump version.

--- frontmacs-pkg.el
+++ frontmacs-pkg.el
@@ -1,5 +1,5 @@
 ;; -*- eval: (flycheck-mode -1) -*-
-(define-package "frontmacs" "0.1.1" "Frontside config package for emacs"
+(define-package "frontmacs" "0.1.2" "Frontside config package for emacs"
   '((magit "2.8.0")
     (swiper "0.7.0")
     (projectile "0.13.0")

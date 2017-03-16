ec8a60e44fa2a9e60d3ba189b86fb37e0af14f6f
Charles Lowell <cowboyd@frontside.io>
2017-03-16 12:40:56 -0500

    add silver searcher as dependency.
    Projectile need this in order to function.

--- frontmacs-pkg.el
+++ frontmacs-pkg.el
@@ -1,9 +1,10 @@
 ;; -*- eval: (flycheck-mode -1) -*-
-(define-package "frontmacs" "0.1.3" "Frontside config package for emacs"
+(define-package "frontmacs" "0.1.4" "Frontside config package for emacs"
   '((f "0.19.0")
     (magit "2.8.0")
     (swiper "0.7.0")
     (projectile "0.13.0")
+    (ag "0.4.7")
     (exec-path-from-shell "1.11")
     (page-break-lines "0.11"))
 

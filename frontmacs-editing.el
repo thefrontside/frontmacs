92c487b2e62c514371d609082af6486982aa77d4
robdel12 <robertdeluca19@gmail.com>
2017-03-17 15:36:52 -0500

    Don't use `define-fringe-bitmap` in terminal

--- frontmacs-editing.el
+++ frontmacs-editing.el
@@ -32,8 +32,10 @@
 (setq flycheck-indication-mode 'right-fringe)
 
 ;; make the flycheck arrow look like a little triagle.
-(define-fringe-bitmap 'flycheck-fringe-bitmap-double-arrow
-  [0 0 0 0 0 4 12 28 60 124 252 124 60 28 12 4 0 0 0 0])
+;; but only do it when emacs runs in a window, not terminal
+(when window-system
+  (define-fringe-bitmap 'flycheck-fringe-bitmap-double-arrow
+    [0 0 0 0 0 4 12 28 60 124 252 124 60 28 12 4 0 0 0 0]))
 
 (add-hook 'prog-mode-hook 'flycheck-mode)
 

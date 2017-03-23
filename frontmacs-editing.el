c38d525bfea6d706e8bf74db23463325f6287080
Charles Lowell <cowboyd@frontside.io>
2017-03-23 15:51:06 -0500

    disable lockfiles
    Emacs creates lockfiles to recognize when someone else is already
    editing the same file as you, but Ember-CLI doesn't know what to do with
    these lock files. One second they are there and the next the lock file
    disappears. This causes issues with Ember-CLI's livereload feature
    where you will commonly get an error like: Error: ENOENT, no such file
    or directory '.../components/.#file-name.hbs'.
    
    Let's just disable them, and see if we ever miss them ever.

--- frontmacs-editing.el
+++ frontmacs-editing.el
@@ -99,12 +99,25 @@
 (require 'which-key)
 (which-key-mode +1)
 
-
 ;; Yet another snippet library, which is awesome. Allows you to expand
 ;; commonly used code templates into your buffer. Use it everywhere!
 ;; see https://joaotavora.github.io/yasnippet/
 (require 'yasnippet)
 (yas-global-mode +1)
+
+;; Emacs creates lockfiles to recognize when someone else is already
+;; editing the same file as you.
+;;
+;; Ember-CLI doesn't know what to do with these lock files. One second
+;; they are there and the next the lock file disappears. This causes
+;; issues with Ember-CLI's livereload feature where you will commonly
+;; get an error like:
+;;
+;; Error: ENOENT, no such file or directory '.../components/.#file-name.hbs'
+;;
+;; To solve this issue we set "create-lockfiles" to nil and it will no
+;; longer create these lock files.
+(setq create-lockfiles nil)
 
 (provide 'frontmacs-editing)
 

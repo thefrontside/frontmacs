2f68033d1ac5a3a61b0adf95604b73978ba226db
robdel12 <robertdeluca19@gmail.com>
2017-04-02 13:22:39 -0500

    Add markdown mode

---
+++ frontmacs-markdown.el
@@ -0,0 +1,7 @@
+(require 'markdown-mode)
+
+(add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
+(add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+
+(provide 'frontmacs-markdown)

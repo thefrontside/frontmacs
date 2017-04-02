adab29584777cfb4f1856fed4be28a26bec1b4b7
robdel12 <robertdeluca19@gmail.com>
2017-04-02 13:39:43 -0500

    Turn on autofill mode when in markdown mode

--- frontmacs-markdown.el
+++ frontmacs-markdown.el
@@ -3,5 +3,6 @@
 (add-to-list 'auto-mode-alist '("\\.markdown\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("\\.md\\'" . markdown-mode))
 (add-to-list 'auto-mode-alist '("README\\.md\\'" . gfm-mode))
+(add-hook 'markdown-mode-hook 'turn-on-auto-fill)
 
 (provide 'frontmacs-markdown)

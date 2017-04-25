7185d0879020b77343696223a8a5ca0415096733
Charles Lowell <cowboyd@frontside.io>
2017-04-25 00:20:59 -0500

    add enhancements for Ruby. (#65)
    The ruby experience was pretty bare, and so this brings in several
    upgrades, mostly from Prelude to make:
    
    - working with rubinius more pleasant
    - use inf-ruby for better completion and code insight
    - add cool Ruby editor tricks with ruby-tools.el

--- frontmacs-ruby.el
+++ frontmacs-ruby.el
@@ -18,4 +18,35 @@
 (add-to-list 'auto-mode-alist '("Berksfile\\'" . ruby-mode))
 (add-to-list 'auto-mode-alist '("Appraisals\\'" . ruby-mode))
 
+;; We never want to edit Rubinius bytecode
+(add-to-list 'completion-ignored-extensions ".rbc")
+
+
+;; Look up symbols in ruby `ri' to using yari.
+(define-key 'help-command (kbd "R") 'yari)
+
+
+;; setup ruby buffers
+(defun frontmacs-ruby-mode-hook ()
+
+  ;; enable a REPL process loaded with your
+  ;; ruby project that provides lots of code insight.
+  ;; https://github.com/nonsequitur/inf-ruby
+  (inf-ruby-minor-mode +1)
+
+  ;; enable handy ruby tools.
+  ;; https://github.com/rejeep/ruby-tools.el
+  (ruby-tools-mode +1)
+
+  ;; CamelCase aware editing operations
+  (subword-mode +1))
+
+(add-hook 'ruby-mode-hook 'frontmacs-ruby-mode-hook)
+
+;; load snippets for writing tests in rspec whenever rspec-mode
+;; is in effect
+(eval-after-load 'rspec-mode
+ '(rspec-install-snippets))
+
+
 (provide 'frontmacs-ruby)

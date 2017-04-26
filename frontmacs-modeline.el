a29c5f64b97731fa891c227bd41c43fc80626e05
Wil Wilsman <wil@frontside.io>
2017-04-26 14:38:57 -0500

    Better comments for modeline setup

--- frontmacs-modeline.el
+++ frontmacs-modeline.el
@@ -2,11 +2,18 @@
 ;;;
 ;;; Commentary:
 ;;;
+;;; The default Emacs modeline is lame to the point of not providing any useful
+;;; information in a sufficiently customized Emacs. The goal is to make our
+;;; modeline useful and beautiful (in that order)
+;;;
 ;;; Code:
 
 (require 'diminish)
 
-;; diminish some modes
+;; Out of the box, Every single major and minor mode gets put onto the modeline
+;; which clutters it up terribly. Using diminish we can hide or abbreviate modes
+;; that don't contribute meaningfully to the high-level context.
+;; see https://github.com/myrjola/diminish.el
 (diminish 'ivy-mode)
 (diminish 'company-mode)
 (diminish 'smartparens-mode)

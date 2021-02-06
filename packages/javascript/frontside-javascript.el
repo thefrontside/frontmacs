2b0e27a2f5fa18079b00753b3bf9635818e11f71
Charles Lowell <cowboyd@frontside.io>
2021-02-06 22:08:15 +0200

    Correct homepage URL
    resolves #145

--- packages/javascript/frontside-javascript.el
+++ packages/javascript/frontside-javascript.el
@@ -7,7 +7,7 @@
 ;; Version: 1.0.0
 ;; Package-Requires: ((emacs "25.1") (add-node-modules-path "1.2.0") (company "0.9.2") (flycheck "20201228.2104") (js2-mode "20201220") (js2-refactor "0.9.0") (rjsx-mode "0.5.0") (tide "4.0.2") (web-mode "17"))
 ;; Keywords: files, tools
-;; URL: https://github.com/frontside/frontmacs
+;; URL: https://github.com/thefrontside/frontmacs
 
 ;;; Commentary:
 

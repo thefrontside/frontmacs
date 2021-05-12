5d8022ec99cd3b6c3ad12830da2e0702a5d4378f
Charles Lowell <cowboyd@frontside.io>
2021-05-12 18:47:21 -0500

    Archive old packages

--- scripts/build-archive.el
+++
@@ -1,3 +0,0 @@
-(require 'package-x)
-(setq package-archive-upload-base (concat default-directory "dist/packages"))
-(package-upload-file (car (file-expand-wildcards "dist/frontmacs-*.tar")))
--- test/fixtures/boot-sequence/config.el
+++
@@ -1,5 +0,0 @@
-;; -*- eval: (flycheck-mode -1) -*-
-(defvar t/boot-sequence (list
-                         :hello "config"
-                         :file frontmacs-config-file
-                         :done (featurep 'frontmacs)))
